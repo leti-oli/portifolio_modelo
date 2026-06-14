2b562a02585ebd16e0a0b1da588da9dc98f90e89
leti-oli <o.leticiaa29@gmail.com>
2026-06-14 18:35:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("btnCuriosidade");
+const texto = document.getElementById("surpresa");
+
+botao.addEventListener("click", function(){
+
+    texto.innerHTML =
+    "🌌 Curiosidade surpresa: escreva aqui uma curiosidade secreta sobre você!";
+
+});
